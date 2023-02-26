58a2bcfc8de74593a15c48c4be9d9b55da98649a
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-02-26 14:56:08 +1100

    Remove dotenv testing code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-require("dotenv").config();
+/* Commenting out Express functions until vanilla JS functionality built */
 
+/*
 const express = require("express"); /* Imports express module into index.js */
 
-console.log(process.env.DB_HOST);
+/*
+require("dotenv").config();
+*/
+
+/* console.log(process.env.DB_HOST); Remove this after you have tested it is working*/
 
 console.log("node started");
+alert("js is working");
