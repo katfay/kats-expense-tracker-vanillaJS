09d253e62a5afd1dbd381261e73d503ada5e4ba3
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-02-26 15:09:37 +1100

    Add comments

--- src/index.js
+++ src/index.js
@@ -9,13 +9,19 @@ require("dotenv").config();
 
 /* console.log(process.env.DB_HOST); Remove this after you have tested it is working*/
 
+// Check node modules are running
 console.log("node started");
+
+// Check index.js is connected to index.html
 console.log("js is working");
 
+// Assign a variable name to the 'Add' button on the dashboard
 let addButton = document.getElementById("#addButton");
 
+// Assign a click event listener, to the 'Add' button, that calls a function to handle the click
 addButton.addEventListener("click", handleAddButtonClick);
 
+// When the 'Add' button is clicked, XXX
 function handleAddButtonClick() {
-  alert("You clicked the add button");
+  console.log("You clicked the add button");
 }
