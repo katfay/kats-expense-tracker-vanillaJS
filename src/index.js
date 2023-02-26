8efc4f5c4beb357022af6ec3bdc77ccbbb686bb9
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-02-26 15:44:22 +1100

    Target pop up wrap display CSS

--- src/index.js
+++ src/index.js
@@ -18,13 +18,13 @@ console.log("js is working");
 // Assign a variable name to the 'Add' button on the dashboard
 let addButton = document.getElementById("#addButton");
 
-// Assign a variable name to the div wrapping the 'Add an expense' popup
-let popUpWrap = document.getElementById("#popUpWrap");
-
 // Assign a click event listener, to the 'Add' button, that calls a function to handle the click
 addButton.addEventListener("click", handleAddButtonClick);
 
 // When the 'Add' button is clicked, XXX
 function handleAddButtonClick() {
   console.log("You clicked the add button");
+  // Assign a variable name to the div wrapping the 'Add an expense' popup
+  let popUpWrap = document.getElementById("#popUpWrap");
+  popUpWrap.style.display = "none";
 }
