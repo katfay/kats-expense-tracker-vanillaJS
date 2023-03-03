880678953aab0faf9e15415857be9d2df8375eaa
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-03-04 09:14:29 +1100

    Updates with Sam help

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ function showAddButton() {
   console.log("show Add button has been called");
 
   /* Assign a variable within this function scope to the 'Add' button wrapper */
-  let addButtonWrap = document.getElementById("#addButtonWrap");
+  let addButtonWrap = document.getElementById("addButtonWrap");
 
   /* Change the CSS display setting to show the 'Add' button on the dashboard */
   addButtonWrap.style.display = "flex";
@@ -16,7 +16,7 @@ function showAddButton() {
 /* Hide the Add an Expenses popup on the dashboard when the 'Add Expense' button is clicked */
 function hidePopUp() {
   /* Assign a variable name to the div wrapping the 'Add an expense' popup within this scope */
-  let popUpWrap = document.getElementById("#popUpWrap");
+  let popUpWrap = document.getElementById("popUpWrap");
 
   /* Change the CSS display setting to hide the pop up on the dashboard */
   popUpWrap.style.display = "none";
@@ -36,7 +36,7 @@ function handleAddExpenseButtonClick(event) {
 /* Show the Add an Expenses popup on the dashboard when the 'Add' button is clicked */
 function showPopUp() {
   /* Assign a variable name to the div wrapping the 'Add an expense' popup */
-  let popUpWrap = document.getElementById("#popUpWrap");
+  let popUpWrap = document.getElementById("popUpWrap");
 
   /* Change the CSS display setting to show the pop up on the dashboard */
   popUpWrap.style.display = "flex";
@@ -48,7 +48,7 @@ function handleAddButtonClick() {
   console.log("You clicked the add button");
 
   /* Assign a variable to the 'Add' button wrapper */
-  let addButtonWrap = document.getElementById("#addButtonWrap");
+  let addButtonWrap = document.getElementById("addButtonWrap");
 
   /* Change the CSS display setting to show the pop up on the dashboard */
   addButtonWrap.style.display = "none";
@@ -58,13 +58,23 @@ function handleAddButtonClick() {
 }
 
 /* Assign a variable name to the 'Add' button on the dashboard */
-let addButton = document.getElementById("#addButton");
+let addButton = document.getElementById("addButton");
 
 /* Assign a click event listener, to the 'Add' button, that calls a function to handle the click */
 addButton.addEventListener("click", handleAddButtonClick);
 
 /* Assign a click event listener to the 'Add Expense' button inside the popup */
-let addExpenseButton = document.getElementById("#expense");
+let addExpenseButton = document.getElementById("addExpense");
 
 /* Assign a submit event listener, to the 'Add Expense' button, that calls a function to handle the click */
-addExpenseButton.addEventListener("submit", handleAddExpenseButtonClick);
+addExpenseButton.addEventListener("click", hidePopUp);
+
+document.getElementById("showButton").addEventListener("click", showExpenses);
+
+function showExpenses() {
+  fetch("http://localhost:3000/expense/list")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
