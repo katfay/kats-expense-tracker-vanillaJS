0fe475fd7ad5fa63742fc0fcfe704642881445a5
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-03-04 14:07:47 +1100

    Add forEach loops to show database items

--- js/index.js
+++ js/index.js
@@ -71,12 +71,38 @@ addExpenseButton.addEventListener("click", hidePopUp);
 
 document.getElementById("showButton").addEventListener("click", showExpenses);
 
+/*function prettifyExpenses(data) {
+
+  });
+}
+*/
+
 function showExpenses() {
   fetch("http://localhost:3000/expense/list")
     .then((res) => res.json())
     .then((data) => {
+      /*
+      let spending = console.log(data);
+            spending.forEach(function (spend) {
+        alert("I have bought " + data[0].expense);
+      });
+*/
+      console.table(data);
       console.log(data);
-      alert(data);
-      document.getElementById("dashboardResults").innerHTML = data;
+      document.getElementById("dashboardResults").innerHTML = data[0].expense;
+      for (let x = 0; x < data.length; x++) {
+        console.log(data[x].expense);
+        /*        document.getElementById("dashboardResults").innerHTML =
+          data[x].expense + " help, I need to append a new line";
+          */
+        let dashboardElement = document.getElementById("dashboardResults");
+        let dashboardHTML = `<div>`;
+        data.forEach(function (expenseItem) {
+          dashboardHTML = dashboardHTML + `<div>${expenseItem.amount}</div>`;
+        });
+        dashboardHTML = dashboardHTML + `</div>`;
+        dashboardElement.innerHTML = dashboardHTML;
+        console.log(dashboardHTML);
+      }
     });
 }
